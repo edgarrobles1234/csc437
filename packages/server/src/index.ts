cb0fab79020f426ccd8b1ebc5b836ec3f6a82a4f
Edgar Robles <edgarrobles@Edgars-MacBook-Pro-4.local>
2026-05-21 11:57:35 -0700

    more bugs

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -19,6 +19,6 @@ app.get("/hello", (_req: Request, res: Response) => {
   res.send("Hello, World");
 });
 
-app.listen(port, "127.0.0.1", () => {
-  console.log(`Server running at http://127.0.0.1:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
